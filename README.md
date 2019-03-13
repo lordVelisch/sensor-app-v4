c98d4aeae1358b7f0e71b95a8c260ff7a2c28335
Valentin Mayer <may17576@spengergasse.at>
2019-03-13 19:00:15 +0100

    Initial commit: Contains Auto-generated project and gitignore as well as readme

---
+++ README.md
@@ -0,0 +1,6 @@
+# sensorapp_v4
+
+A sensor app works with GPS Data. Part of school exercise
+
+More information: Pos Android, POS-07-AA-04
+
---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(title: 'SensorApp v4'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        elevation: 1.0,
+      ),
+      body: Center(
+        child: Text("My location will be here"),
+      ),
+    );
+  }
+}
